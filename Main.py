822429ad32441f308adb3945b7b85950273f64be
Ian <supergachaworld4@gmail.com>
2025-07-22 13:32:06 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
-class object(self, tipo, vida, daño):
-    def __init__: 
-        self.tipo = tipo
-        if self.tipo == "jugador":
-            self.vida = int(vida)
-            self.daño = int(daño)
-        elif self.tipo == "bot":
-            self.vida = int(vida)
-            self.daño = int(personaje.daño / 2)
-        elif self.tipo == "pared":
-            self.vida == int(int(vida) + int(daño))
-            self.daño == 0
-        else:
-            self.vida = int(vida)
-            self.daño = int(daño)
-
 comando = str(input("cmd python:/main.py: ")
-
-if comando = "new.object": 
+commands = ["n es primo", "n es impar o par"]
+a = 0
+while comando == stop
+    if comando in commands:
+        if comando == "n es impar o par":
+            n = int(input("n: ")
+            if n % 2 == 1:
+                    return "impar"
+            elif n % 2 == 0:
+                    return"par"
+            elif comando == "n es primo":
+                    while a == n:
+                        a = a + 1
+                        b = int(n / a)
+                        if type(b) = "int" :
+                            return "n no es primo"
+                            break
+                        
+                        
+        
     
